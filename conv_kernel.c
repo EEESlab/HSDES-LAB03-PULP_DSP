248d783834c08ca801c6d16acc960b456cd964aa
Francesco Conti <f.conti@unibo.it>
2020-04-19 23:46:05 +0200

    Added license headers

--- conv_kernel.c
+++ conv_kernel.c
@@ -1,6 +1,30 @@
+/*
+ * Copyright (C) 2015-2020 ETH Zurich and University of Bologna
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <stdio.h>
 #include <stdint.h>
 
+/*
+  Inputs, weights and outputs are represented in fixed-point Q1.7 unsigned format:
+  this means that each integer in [0-255] represents a real value in the range [0.0-1.0)
+  The relationship between the integer I and real R representations is given by
+
+     R = I * 2^-FRACTIONARY_BITS
+
+ */
 #define FRACTIONARY_BITS 7
 #define ROUNDBIT   (1 << (FRACTIONARY_BITS -1))
 #define SATURATION 255
@@ -34,8 +58,6 @@ void __attribute__ ((noinline)) ConvKxK_Naive  (uint8_t * In_Img, uint8_t * Out_
             }
         }
 
-        // Rounding
-        // S = S + ROUNDBIT;
         // Normalization: Data are Q2.2*(FRACTIONARY_BITS-1), now Q2.FRACTIONARY_BITS-1
         S = S >> FRACTIONARY_BITS;
         // Saturation
